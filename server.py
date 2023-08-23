97a9500d7cd4b461d9c2e3a180ad510f6b29c063
Thanmayi S Hegde <thanmayishegde2002@gmail.com>
2023-08-23 20:35:19 +0530

    my first commit

--- server.py
+++ server.py
@@ -1,19 +1,30 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world',200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json
+    f_no = data['first']
+    s_no = data['second']
+    result = f_no + s_no
+    res = {'result':result}
+    return jsonify(res),200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.json
+    f_no = data['first']
+    s_no = data['second']
+    result = f_no - s_no
+    res = {'result':result}
+    return jsonify(res),200
+   
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
